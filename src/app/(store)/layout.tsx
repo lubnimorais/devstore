472465b7cf64f54a818c714bca819b25fc0e7a0a
Lubni Morais <lubni.morais@gmail.com>
2025-04-04 18:00:17 -0300

    feat: component Header

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -3,7 +3,7 @@ import type { ReactNode } from 'react';
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
-    <div>
+    <div className="min-h-screen w-full max-w-[1600px] mx-auto grid grid-rows-[min-content_max-content] gap-5 p-8">
       <Header />
 
       {children}
